c1fa972157a341fc2a14998203cc20ec4d0774ea
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2025-02-04 20:14:58 +0700

    update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,11 +1,3 @@
-const pino = require("pino")
-const baileys = require("@whiskeysockets/baileys")
-const {readFileSync,existsSync,writeFileSync,rmSync} = require("fs")
-const {exec} = require("child_process")
-const axios = require("axios")
-const { fileTypeFromBuffer } = await import("file-type")
-const { randomBytes } = require("crypto")
-
 /*require("../node_modules/@whiskeysockets/baileys/lib/Utils/generics.js").generateMessageID = () => {
     return require('crypto').randomBytes(14).toString('hex').toUpperCase() + '-FRM'
 }*/
