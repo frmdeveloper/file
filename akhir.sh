a3ade02b914e877b7957681c7ceca7184675925d
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2025-02-07 19:48:10 +0700

    update akhir.sh

--- akhir.sh
+++ akhir.sh
@@ -3,6 +3,7 @@ if [ ! -d "WAProto" ]; then
   echo "{}" > WAProto/package.json
 fi
 rm -rf package-lock.json
-curl -s 'https://raw.githubusercontent.com/wppconnect-team/wa-proto/refs/heads/main/dist/index.js' | sed 's/waproto/proto/g' > 'WAProto/index.js'
-curl -s 'https://raw.githubusercontent.com/wppconnect-team/wa-proto/refs/heads/main/dist/index.d.ts' | sed 's/waproto/proto/g' > 'WAProto/index.d.ts'
-curl -s 'https://raw.githubusercontent.com/wppconnect-team/wa-proto/refs/heads/main/WAProto.proto' | sed 's/waproto/proto/g' > 'WAProto/WAProto.proto'
+echo "Download WAProto..."
+curl 'https://raw.githubusercontent.com/wppconnect-team/wa-proto/refs/heads/main/dist/index.js' | sed 's/waproto/proto/g' > 'WAProto/index.js'
+curl 'https://raw.githubusercontent.com/wppconnect-team/wa-proto/refs/heads/main/dist/index.d.ts' | sed 's/waproto/proto/g' > 'WAProto/index.d.ts'
+curl 'https://raw.githubusercontent.com/wppconnect-team/wa-proto/refs/heads/main/WAProto.proto' | sed 's/waproto/proto/g' > 'WAProto/WAProto.proto'
